4b7b9387bb4b9e1cf5dff22989fa55b1a78cea27
SadLoneWolf <djordjeandjic99@gmail.com>
2021-10-16 11:54:04 +0300

    Start to work with animation:

--- MMPP1/app/src/main/java/com/example/mmpp1/Animation.kt
+++ MMPP1/app/src/main/java/com/example/mmpp1/Animation.kt
@@ -3,12 +3,15 @@ package com.example.mmpp1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Animation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animation)
         val detailNumber = intent.getIntExtra("EXTRA_NUMBER",1)
+        val text = findViewById<TextView>(R.id.textView3)
+        text.text = detailNumber.toString()
     }
 }
 
