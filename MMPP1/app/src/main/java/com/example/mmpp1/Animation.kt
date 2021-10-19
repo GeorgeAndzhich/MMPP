76dac75523c5f2c0449de1b0fa31f993a3c22f2e
SadLoneWolf <djordjeandjic99@gmail.com>
2021-10-19 13:17:05 +0300

    refactoring

--- MMPP1/app/src/main/java/com/example/mmpp1/Animation.kt
+++ MMPP1/app/src/main/java/com/example/mmpp1/Animation.kt
@@ -12,49 +12,49 @@ class Animation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animation)
-        val detailNumber = intent.getIntExtra("EXTRA_NUMBER",1)
+        val detailNumber = intent.getIntExtra("EXTRA_NUMBER", 1)
         val text = findViewById<TextView>(R.id.textView3)
         text.text = detailNumber.toString()
-        var count =   text.text.toString().toInt()
+        var count = text.text.toString().toInt()
 
 
         /*Инициализация начальных данных и структур*/
         var doneItems = ArrayList<Item>()
         var finalItems = ArrayList<Item>()
         var itemList = ArrayList<Item>()
-        for (count in itemList.listIterator()){
-            var item = Item(false,false)
+        for (count in itemList.listIterator()) {
+            var item = Item(false, false)
             itemList.add(item)
         }
         var machine1 = Machine()
         var machine2 = Machine2()
 
         /*Главный цыкл*/
-            for (item in itemList.indices)
-            itemList.forEach {
+        for (item in itemList.indices){
                 machine1.increaseCount(machine1.itemCount)
                 machine2.startMachine(itemList)
                 machine1.itemDone(itemList[item])
-                machine2.itemDone(itemList[item+1]) /*Ошибка - одну деталь одновременно обрабатывают два станка*/ /*Ошибка обработана!!!*/
-                if (itemList[item].done1 == true and itemList[item].done2 == false ){
-                        doneItems.add(itemList[item])
-                        doneItems.remove(itemList[item])
-                    } else if (itemList[item].done1 == true and itemList[item].done2 == true){
-                            doneItems.add(itemList[item])
-                            finalItems.add(itemList[item])
-                            doneItems.remove(itemList[item])
-                            itemList.remove(itemList[item])
-                    }
+                machine2.itemDone(itemList[item + 1]) /*Ошибка - одну деталь одновременно обрабатывают два станка*/ /*Ошибка обработана!!!*/
+                if (itemList[item].done1 == true and itemList[item].done2 == false) {
+                    doneItems.add(itemList[item])
+                    doneItems.remove(itemList[item])
+                } else if (itemList[item].done1 == true and itemList[item].done2 == true) {
+                    doneItems.add(itemList[item])
+                    finalItems.add(itemList[item])
+                    doneItems.remove(itemList[item])
+                    itemList.remove(itemList[item])
                 }
-                if (itemList.size == 0){
-                    TODO("Логика расчета показателей")
-                    /*Здесь понадобятся т, время которе прошло.
+
+            if (itemList.size == 0) {
+                TODO("Логика расчета показателей")
+                /*Здесь понадобятся т, время которе прошло.
                     * Формулы для расчета:
                     * Производительность [П]
                     *   П = сделанные / время которое прошло
                     * Уровень брака [Б]:
                     * Б = (совокупная бракованность деталей / сумма брака станков )*100*/
-                }
-       }
+            }
+        }
+    }
    }
 
